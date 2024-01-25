65cc71d6cdc04134bf8f102077e82d5f3f44946e
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-01-25 16:34:51 +0530

    Backend is completed but 3 issues needs to be resolved

--- to-do/toDoApp/backend/api/views.py
+++ to-do/toDoApp/backend/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.authtoken.models import Token
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
 # Create your views here.
 class TodoList(generics.ListAPIView):
     serializer_class=TodoSerializer
@@ -42,21 +43,36 @@ class TodoToggleComplete(generics.UpdateAPIView):
         serializer.save()
     
     
-    @csrf_exempt 
-    def signup(request):
-        if request.method == 'POST':
+@csrf_exempt 
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data= JSONParser().parse(request)
+            user = User.objects.create_user(
+                username= data['username'],
+                passoword = data['password']
+            )
+            user.save()
+            
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)}, status = 201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error':'username taken. choose another username'},
+                status=400
+            )
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        data= JSONParser().parse(request)
+        user= authenticate(request, username = data["username"],
+                           password=data["password"])
+        if user is None:
+            return JsonResponse({'error': 'unable to login. Please check username and password'}, status = 400)
+        else:
             try:
-                data= JSONParser().parse(request)
-                user = User.objects.create_user(
-                    username= data['username'],
-                    passoword = data['password']
-                )
-                user.save()
-                
+                token=Token.objects.get(user=user)
+            except:
                 token = Token.objects.create(user=user)
-                return JsonResponse({'token':str(token)}, status = 201)
-            except IntegrityError:
-                return JsonResponse(
-                    {'error':'username taken. choose another username'},
-                    status=400
-                )
+            return JsonResponse({'token': str(token)}, status = 201)
+        
